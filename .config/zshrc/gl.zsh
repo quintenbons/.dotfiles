171802fa9faaa55ab5e47ce4ec7a1a3f06b51b3b
quintenbons <quintenbons@outlook.com>
2023-01-03 20:47:11 +0100

    gl

--- .config/zshrc/gl.zsh
+++ .config/zshrc/gl.zsh
@@ -2,9 +2,9 @@ JAVA_HOME=/usr/lib/jvm/java-1.18.0-openjdk-amd64 # A Adapter
 M2_HOME=/opt/maven                               # A adapter
 MAVEN_HOME=$M2_HOME
 PATH=$M2_HOME/bin:"$PATH"
-PATH=$HOME/Documents/school/gl16/global/bin:"$PATH"
+PATH=$HOME/Documents/school/gl/global/bin:"$PATH"
 PATH=$JAVA_HOME/bin:"$PATH"
-PATH=$HOME/Documents/school/gl16/src/main/bin:"$PATH"
-PATH=$HOME/Documents/school/gl16/src/test/script:"$PATH"
-PATH=$HOME/Documents/school/gl16/src/test/script/launchers:"$PATH"
+PATH=$HOME/Documents/school/gl/gl16/src/main/bin:"$PATH"
+PATH=$HOME/Documents/school/gl/gl16/src/test/script:"$PATH"
+PATH=$HOME/Documents/school/gl/gl16/src/test/script/launchers:"$PATH"
 export PATH
